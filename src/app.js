8f432216af854d1a9184350796221c6efbed94b2
Python Development <python@Hungs-MacBook-Pro.local>
2020-10-17 21:15:39 -0700

    add routes for express

--- src/app.js
+++ src/app.js
@@ -1,4 +1,21 @@
 import 'dotenv/config.js';
+import express from 'express';
 
-console.log('hello');
-console.log(process.env.MY_SECRET);
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', function (req, res) {
+    res.json({ msg: 'Hello' });
+});
+
+app.get('/hello', function (req, res) {
+    res.json({ msg: 'Hello' });
+});
+
+app.get('/dashboard', function (req, res) {
+    res.json({ msg: 'Hello' });
+});
+
+app.listen(port, function () {
+    console.log('express server backend running on port', port);
+});
